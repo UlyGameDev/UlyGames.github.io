8f2807105e118c8c6a98639e2818e23502156895
dhayesboi <Shahd1@kgv.hk>
2021-07-16 16:20:20 +0800

    titlebar

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import './App.css';
 
 function App() {
   return (
+
     <div className="App">
       <header className="App-header">
+        <link rel="icon" href={logo} />
+        <title>UlyGames</title>
+        <meta property="og:title" content="UlyGames" />
+        <meta property="og:description" content="Homepage for UlyGames" />
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
           Welcome to UlyGames!
         </h1>
       </header>
       <body>
-      <iframe src="https://discord.com/widget?id=865431663638347787&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+        <iframe src="https://discord.com/widget?id=865431663638347787&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
       </body>
     </div>
   );
