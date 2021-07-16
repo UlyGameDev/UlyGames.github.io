0400dc209dcf026c4fba708bdfc820dd333469f9
dhayesboi <Shahd1@kgv.hk>
2021-07-16 16:05:05 +0800

    update

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>
           Welcome to UlyGames!
-        </p>
+        </h1>
       </header>
+      <body>
+      <iframe src="https://discord.com/widget?id=865431663638347787&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+      </body>
     </div>
   );
 }
