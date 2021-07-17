4a16bd8a6c6be533c33a4f75a732949e58455842
dhayesboi <Shahd1@kgv.hk>
2021-07-17 14:10:53 +0800

    better discord widget moment?

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ function App() {
         <h1>
           Welcome to UlyGames!
         </h1>
+        <iframe class="discord" src="https://e.widgetbot.io/channels/2865431663638347787/865804908917424138" height="600" width="800"></iframe>
       </header>
       <body>
-        <iframe class="discord" src="https://discord.com/widget?id=865431663638347787&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
       </body>
     </div>
   );
