417da696a2c0f3243964bd13038903c8994bfcee
dhayesboi <Shahd1@kgv.hk>
2021-07-17 21:46:21 +0800

    better widget

--- src/discord.js
+++ src/discord.js
@@ -9,7 +9,7 @@ function Disc() {
         <img class="logo" src="https://i.imgur.com/7OUcNKL.png" alt="ulyLogo" />
 
         <div class="discordwidget">
-          <iframe src="https://e.widgetbot.io/channels/2865431663638347787/865804908917424138" height="600" width="600"></iframe>
+          <iframe src="https://discord.com/widget?id=865431663638347787&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
         </div>
         <div class="txtr"><h2>Communicate with your enemies.</h2></div>
         <div class="txtr"><h4>Join the official Uly Games Discord Server to not only be the first to know whenever an official update releases, but also to start and join voice, chat and parties to experience fun and amazing games with your friends.</h4></div>
